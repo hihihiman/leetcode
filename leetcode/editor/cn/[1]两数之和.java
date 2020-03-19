2888ee8d78b189066cda6422fa818a550644f269
dujianghui <dujianghui537885@163.com>
2020-03-19 13:21:46 +0800

    3 easy and 2 medium

--- leetcode/editor/cn/[1]两数之和.java
+++ leetcode/editor/cn/[1]两数之和.java
@@ -15,7 +15,15 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0;i<nums.length;i++){
+            int sub = target-nums[i];
+            if (map.containsKey(sub)){
+                return new int[]{i,map.get(sub)};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
