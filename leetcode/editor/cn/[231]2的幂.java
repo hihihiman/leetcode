96f2ec206b6777908001175111c25cd4dac4a019
dujianghui <dujianghui537885@163.com>
2020-03-27 10:41:29 +0800

    No.231 这题是真的帅

--- leetcode/editor/cn/[231]2的幂.java
+++ leetcode/editor/cn/[231]2的幂.java
@@ -22,7 +22,7 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public boolean isPowerOfTwo(int n) {
-
+       return n<1?false:(n&(n-1))==0;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
