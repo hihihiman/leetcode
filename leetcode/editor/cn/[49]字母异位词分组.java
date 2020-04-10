f9e0e3db36ab476ff36d8181e45369284b29e7f8
dujianghui <dujianghui537885@163.com>
2020-04-10 21:44:36 +0800

    No.53 hot list

--- leetcode/editor/cn/[49]字母异位词分组.java
+++ leetcode/editor/cn/[49]字母异位词分组.java
@@ -22,7 +22,25 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
+        HashMap<String,List<String>> hash = new HashMap<>();
+        for (int i=0;i<strs.length;i++){
+            int[] num = new int[26];
+            for (int j=0;j<strs[i].length();j++){
+                num[strs[i].charAt(j)-'a']++;
+            }
+            String key = "";
+            for (int j=0;j<num.length;j++){
+                key = key+num[j]+'#';
+            }
+            if (hash.containsKey(key)){
+                hash.get(key).add(strs[i]);
+            }else {
+                List<String> temp = new ArrayList<String>();
+                temp.add(strs[i]);
+                hash.put(key,temp);
+            }
+        }
+        return new ArrayList<List<String>>(hash.values());
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
