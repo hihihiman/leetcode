db912b8f35919b8a50a6bfc974e7f9f63b691a95
dujianghui <dujianghui537885@163.com>
2020-04-10 21:06:51 +0800

    rotate image

--- leetcode/editor/cn/[48]旋转图像.java
+++ leetcode/editor/cn/[48]旋转图像.java
@@ -47,15 +47,22 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
+        int n = matrix[0].length;
+        //转置
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                int t=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=t;
+            }
+        }
 
-        for (int i=0;i<n/2;i++){
-            for (int j=i;j<n-i-1;j++){
-                int temp = matrix[i][j];
-                matrix[i][j]=matrix[n-j-1][i];
-                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
-                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
-                matrix[j][n-i-1] = temp;
+        //镜像
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n/2;j++){
+                int t = matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=t;
             }
         }
     }
