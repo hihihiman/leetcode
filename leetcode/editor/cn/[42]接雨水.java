0fd4b2616db17fb4eb3ccd93add78567a1e07687
dujianghui <dujianghui537885@163.com>
2020-04-10 17:37:01 +0800

    No.48 hot list

--- leetcode/editor/cn/[42]接雨水.java
+++ leetcode/editor/cn/[42]接雨水.java
@@ -18,7 +18,20 @@ class Solution {
         int sum = 0;
         Stack<Integer> stack = new Stack<>();
         int current = 0;
-        while()
+        while(current<height.length){
+            while (!stack.empty()&&height[current]>height[stack.peek()]){
+                int h = height[stack.peek()];
+                stack.pop();
+                if (stack.empty()){
+                    break;
+                }
+                int distance  = current-stack.peek()-1;
+                int min = Math.min(height[stack.peek()],height[current]);
+                sum += distance*(min-h);
+            }
+            stack.push(current++);
+        }
+        return sum;
     }
 
 }
--- leetcode/editor/cn/[46]全排列.java
+++ leetcode/editor/cn/[46]全排列.java
@@ -18,7 +18,31 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> all = new ArrayList<>();
+        upset(nums, 0, all);
+        return all;
+    }
 
+    private void upset(int[] nums, int begin, List<List<Integer>> all) {
+        if (begin == nums.length) {
+            ArrayList<Integer> temp = new ArrayList<Integer>();
+            for (int i = 0; i < nums.length; i++) {
+                temp.add(nums[i]);
+            }
+            all.add(new ArrayList<Integer>(temp));
+            return;
+        }
+        for (int i = begin; i < nums.length; i++) {
+            swap(nums, i, begin);
+            upset(nums, begin + 1, all);
+            swap(nums, i, begin);
+        }
+    }
+
+    private void swap(int[] nums, int i, int begin) {
+        int temp = nums[i];
+        nums[i] = nums[begin];
+        nums[begin] = temp;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
