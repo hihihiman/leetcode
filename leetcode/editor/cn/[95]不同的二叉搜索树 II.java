fd27a788a05358e2c6e996815e43c6f68d3493af
Josh <dujianghui537885@163.com>
2020-06-03 17:44:49 +0800

    [Tree]No.95

---
+++ leetcode/editor/cn/[95]不同的二叉搜索树 II.java
@@ -0,0 +1,87 @@
+//给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。 
+//
+// 
+//
+// 示例： 
+//
+// 输入：3
+//输出：
+//[
+//  [1,null,3,2],
+//  [3,2,null,1],
+//  [3,1,null,null,2],
+//  [2,1,3],
+//  [1,null,2,null,3]
+//]
+//解释：
+//以上的输出对应以下 5 种不同结构的二叉搜索树：
+//
+//   1         3     3      2      1
+//    \       /     /      / \      \
+//     3     2     1      1   3      2
+//    /     /       \                 \
+//   2     1         2                 3
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 0 <= n <= 8 
+// 
+// Related Topics 树 动态规划
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+//import define.TreeNode;
+//
+//import java.util.LinkedList;
+//import java.util.List;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generateTrees(1, n);
+    }
+
+    private List<TreeNode> generateTrees(int start, int end) {
+        List<TreeNode> res = new LinkedList<>();
+        if (start > end) {
+            res.add(null);
+            return res;
+        }
+        for (int i = start; i <= end; i++) {
+            List<TreeNode> left = generateTrees(start, i - 1);
+            List<TreeNode> right = generateTrees(i + 1, end);
+            for (TreeNode x : left) {
+                for (TreeNode y : right) {
+                    TreeNode node = new TreeNode(i);
+                    node.left = x;
+                    node.right = y;
+                    res.add(node);
+                }
+            }
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
