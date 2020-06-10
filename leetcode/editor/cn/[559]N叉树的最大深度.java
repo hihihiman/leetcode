c5e459908e0ee8e851e3e606e6164b936ac84261
Josh <dujianghui537885@163.com>
2020-06-10 11:18:02 +0800

    [TREE] No.700 easy only

---
+++ leetcode/editor/cn/[559]N叉树的最大深度.java
@@ -0,0 +1,58 @@
+//给定一个 N 叉树，找到其最大深度。 
+//
+// 最大深度是指从根节点到最远叶子节点的最长路径上的节点总数。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 我们应返回其最大深度，3。 
+//
+// 说明: 
+//
+// 
+// 树的深度不会超过 1000。 
+// 树的节点总不会超过 5000。 
+// Related Topics 树 深度优先搜索 广度优先搜索
+
+
+import define.Node;
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+
+class Solution {
+    public int maxDepth(Node root) {
+        if (root == null){
+            return 0;
+        }
+        int depth = 0;
+        for (int i=0;i<root.children.size();i++){
+            depth = Math.max(depth,maxDepth(root.children.get(i)));
+        }
+        return depth+1;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[590]N叉树的后序遍历.java
@@ -0,0 +1,56 @@
+//给定一个 N 叉树，返回其节点值的后序遍历。 
+//
+// 例如，给定一个 3叉树 : 
+//
+// 
+//
+// 
+//
+// 
+//
+// 返回其后序遍历: [5,6,3,2,4,1]. 
+//
+// 
+//
+// 说明: 递归法很简单，你可以使用迭代法完成此题吗? Related Topics 树
+
+import define.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+//leetcode submit region begin(Prohibit modification and deletion)
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+
+
+class Solution {
+    public List<Integer> postorder(Node root) {
+        List<Integer> list = new ArrayList<>();
+        if (root==null){
+            return list;
+        }
+        for (Node node:root.children){
+            list.addAll(postorder(node));
+        }
+        list.add(root.val);
+        return list;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
