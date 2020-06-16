9254523789f1caa53973d5363222a91e0854bac3
Josh <dujianghui537885@163.com>
2020-06-16 15:37:49 +0800

    ???

---
+++ leetcode/editor/cn/[面试题 04.02]最小高度树.java
@@ -0,0 +1,24 @@
+//给定一个有序整数数组，元素各不相同且按升序排列，编写一个算法，创建一棵高度最小的二叉搜索树。示例: 给定有序数组: [-10,-3,0,5,9], 一个可能
+//的答案是：[0,-3,9,-10,null,5]，它可以表示下面这个高度平衡二叉搜索树：           0          / \        -3 
+//  9        /   /      -10  5 Related Topics 树 深度优先搜索
+
+
+import define.TreeNode;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
