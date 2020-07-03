7e668a85330a121b0cac0000c1d3897b3b6f6524
Josh <dujianghui537885@163.com>
2020-07-03 10:07:59 +0800

    [Arrays] No.888 easy only

--- leetcode/editor/cn/[840]矩阵中的幻方.java
+++ leetcode/editor/cn/[840]矩阵中的幻方.java
@@ -38,7 +38,65 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int numMagicSquaresInside(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int res = 0;
+        for (int i = 0; i + 2 < n; i++) {
+            for (int j = 0; j + 2 < m; j++) {
+                res+=judge(i,j,grid);
+            }
+        }
+        return res;
+    }
 
+    private int judge(int i, int j, int[][] grid) {
+        //判断是否是唯一的1-9数字
+        int[] a =new int[10];
+        for (int k = 0; k < i+3; k++) {
+            for (int l = 0; l < j+3; l++) {
+                if (grid[k][l]>=10||grid[k][l]<=0) {
+                    return 0;
+                }
+                a[grid[k][l]]++;
+                if (a[grid[k][l]]>1){
+                    return 0;
+                }
+            }
+        }
+        //判断每一行
+        int sum = -1,tmp=0;
+        for (int k = i; k < i+3; k++) {
+            tmp = 0;
+            for (int l = j; l < j + 3; l++) {
+                tmp+=grid[k][l];
+            }
+            if (sum==-1) {
+                sum=tmp;
+            }else if (sum!=tmp){
+                return 0;
+            }
+        }
+        //判断每一列
+        for (int k = j; k < j+3 ; k++) {
+            tmp=0;
+            for (int l = i; l <i+3 ; l++) {
+                tmp+=grid[k][l];
+            }
+            if (sum!=tmp){
+                return 0;
+            }
+        }
+        //主对角线
+        tmp = grid[i][j]+grid[i+1][j+1]+grid[i+2][j+2];
+        if(sum!=tmp) {
+            return 0;
+        }
+        //副对角线
+        tmp = grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j];
+        if(sum!=tmp) {
+            return 0;
+        }
+        return 1;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
