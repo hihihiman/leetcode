c4f3e43fad58fc794a1419b3a7c24f9171ed68ad
Josh <dujianghui537885@163.com>
2020-07-08 14:08:41 +0800

    [Arrays] No.1464 easy only

---
+++ leetcode/editor/cn/[1389]按既定顺序创建目标数组.java
@@ -0,0 +1,78 @@
+//给你两个整数数组 nums 和 index。你需要按照以下规则创建目标数组： 
+//
+// 
+// 目标数组 target 最初为空。 
+// 按从左到右的顺序依次读取 nums[i] 和 index[i]，在 target 数组中的下标 index[i] 处插入值 nums[i] 。 
+// 重复上一步，直到在 nums 和 index 中都没有要读取的元素。 
+// 
+//
+// 请你返回目标数组。 
+//
+// 题目保证数字插入位置总是存在。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：nums = [0,1,2,3,4], index = [0,1,2,2,1]
+//输出：[0,4,1,3,2]
+//解释：
+//nums       index     target
+//0            0        [0]
+//1            1        [0,1]
+//2            2        [0,1,2]
+//3            2        [0,1,3,2]
+//4            1        [0,4,1,3,2]
+// 
+//
+// 示例 2： 
+//
+// 输入：nums = [1,2,3,4,0], index = [0,1,2,3,0]
+//输出：[0,1,2,3,4]
+//解释：
+//nums       index     target
+//1            0        [1]
+//2            1        [1,2]
+//3            2        [1,2,3]
+//4            3        [1,2,3,4]
+//0            0        [0,1,2,3,4]
+// 
+//
+// 示例 3： 
+//
+// 输入：nums = [1], index = [0]
+//输出：[1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length, index.length <= 100 
+// nums.length == index.length 
+// 0 <= nums[i] <= 100 
+// 0 <= index[i] <= i 
+// 
+// Related Topics 数组 
+// 👍 10 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        int[] target = new int[nums.length];
+        for (int i=0;i<index.length;i++) {
+            insertOne(target,index[i],nums[i]);
+        }
+        return target;
+    }
+
+    private void insertOne(int[] target, int index, int num) {
+        for (int i = target.length-1; i >index ; i--) {
+            target[i] = target[i-1];
+        }
+        target[index] = num;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ leetcode/editor/cn/[1460]通过翻转子数组使两个数组相等.java
@@ -0,0 +1,88 @@
+//给你两个长度相同的整数数组 target 和 arr 。 
+//
+// 每一步中，你可以选择 arr 的任意 非空子数组 并将它翻转。你可以执行此过程任意次。 
+//
+// 如果你能让 arr 变得与 target 相同，返回 True；否则，返回 False 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：target = [1,2,3,4], arr = [2,4,1,3]
+//输出：true
+//解释：你可以按照如下步骤使 arr 变成 target：
+//1- 翻转子数组 [2,4,1] ，arr 变成 [1,4,2,3]
+//2- 翻转子数组 [4,2] ，arr 变成 [1,2,4,3]
+//3- 翻转子数组 [4,3] ，arr 变成 [1,2,3,4]
+//上述方法并不是唯一的，还存在多种将 arr 变成 target 的方法。
+// 
+//
+// 示例 2： 
+//
+// 输入：target = [7], arr = [7]
+//输出：true
+//解释：arr 不需要做任何翻转已经与 target 相等。
+// 
+//
+// 示例 3： 
+//
+// 输入：target = [1,12], arr = [12,1]
+//输出：true
+// 
+//
+// 示例 4： 
+//
+// 输入：target = [3,7,9], arr = [3,7,11]
+//输出：false
+//解释：arr 没有数字 9 ，所以无论如何也无法变成 target 。
+// 
+//
+// 示例 5： 
+//
+// 输入：target = [1,1,1,1,1], arr = [1,1,1,1,1]
+//输出：true
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// target.length == arr.length 
+// 1 <= target.length <= 1000 
+// 1 <= target[i] <= 1000 
+// 1 <= arr[i] <= 1000 
+// 
+// Related Topics 数组 
+// 👍 8 👎 0
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int a :
+                arr) {
+            map.put(a,map.getOrDefault(a,0)+1);
+        }
+        for (int t :
+                target) {
+            if (!map.containsKey(t)||map.get(t)==0){
+                return false;
+            }else {
+                map.put(t,map.get(t)-1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry :
+                map.entrySet()) {
+            if (entry.getValue()>0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
