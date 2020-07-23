e108340aa86c866e550ec7c07201d7cf2374d180
Josh <dujianghui537885@163.com>
2020-07-23 09:35:02 +0800

    [select list]No.130

---
+++ leetcode/editor/cn/[130]被围绕的区域.java
@@ -0,0 +1,65 @@
+//给定一个二维的矩阵，包含 'X' 和 'O'（字母 O）。 
+//
+// 找到所有被 'X' 围绕的区域，并将这些区域里所有的 'O' 用 'X' 填充。 
+//
+// 示例: 
+//
+// X X X X
+//X O O X
+//X X O X
+//X O X X
+// 
+//
+// 运行你的函数后，矩阵变为： 
+//
+// X X X X
+//X X X X
+//X X X X
+//X O X X
+// 
+//
+// 解释: 
+//
+// 被围绕的区间不会存在于边界上，换句话说，任何边界上的 'O' 都不会被填充为 'X'。 任何不在边界上，或不与边界上的 'O' 相连的 'O' 最终都会被
+//填充为 'X'。如果两个元素在水平或垂直方向相邻，则称它们是“相连”的。 
+// Related Topics 深度优先搜索 广度优先搜索 并查集 
+// 👍 264 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    int[] dx = {1, -1, 0, 0};
+    int[] dy = {0, 0, 1, -1};
+
+    public void solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if ((i == 0 || j == 0 || i == board.length - 1 || j == board[0].length - 1) && board[i][j] == 'O') {
+                    helper(board, i, j);
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+                if (board[i][j]=='K'){
+                    board[i][j]='O';6
+                }
+            }
+        }
+    }
+
+    private void helper(char[][] board, int i, int j) {
+        board[i][j] = 'K';
+        for (int k = 0; k < 4; k++) {
+            int x = i + dx[k];
+            int y = j + dy[k];
+            if (x >= 0 && y >= 0 && x < board.length && y < board[0].length && board[x][y] == 'O') {
+                helper(board, x, y);
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
--- leetcode/editor/cn/[64]最小路径和.java
+++ leetcode/editor/cn/[64]最小路径和.java
@@ -19,20 +19,21 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m=grid.length;//row
-        int n=grid[0].length;//column
-        for (int i=1;i<n;i++){
-            grid[0][i] = grid[0][i-1]+grid[0][i];
-        }
-        for (int i=1;i<m;i++){
-            grid[i][0]=grid[i-1][0]+grid[i][0];
-        }
-        for (int i=1;i<m;i++){
-            for (int j=1;j<n;j++){
-                grid[i][j] += Math.min(grid[i][j-1],grid[i-1][j]);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i==0&&j==0){
+                    continue;
+                }
+                if (i==0){
+                    grid[i][j] += grid[i][j-1];
+                }else if (j==0){
+                    grid[i][j]+=grid[i-1][j];
+                }else {
+                    grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1]);
+                }
             }
         }
-        return grid[m-1][n-1];
+        return grid[grid.length-1][grid[0].length-1];
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
